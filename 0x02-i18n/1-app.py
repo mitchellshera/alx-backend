c275dc39951d59cdfdbf4800064f2f6017a95790
mitchellshera <mitchellwachera@gmail.com>
2024-01-09 15:03:13 +0300

    1-app

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+1-app.py
+This module contains a Flask app with Flask-Babel integration.
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+# Instantiate Babel object
+babel = Babel(app)
+
+
+class Config:
+    """
+    Configuration class for Flask app.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# Use Config as config for the Flask app
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """
+    Render the index page.
+
+    Returns:
+        str: Rendered HTML content.
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
