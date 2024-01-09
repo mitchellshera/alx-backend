7483aea32f85079b0bf134c7003674ab96ad58de
mitchellshera <mitchellwachera@gmail.com>
2024-01-09 13:04:05 +0300

    comments

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,9 +5,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """
+    Render the index page.
+
+    Returns:
+        str: Rendered HTML content.
+    """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
