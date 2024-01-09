d438cd2dd6823d548fa9e325298d55bd787351ff
mitchellshera <mitchellwachera@gmail.com>
2024-01-09 21:11:54 +0300

    4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+This module contains a Flask app with Flask-Babel integration,
+language selection, parametrized templates, and support for forcing a particular locale.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+
+# Instantiate Babel object
+babel = Babel(app)
+
+
+class Config:
+    """
+    Configuration class for Flask app.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# Use Config as config for the Flask app
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine the best-matching language from the supported languages.
+
+    Returns:
+        str: Best-matching language code.
+    """
+    forced_locale = request.args.get('locale')
+    if forced_locale and forced_locale in app.config['LANGUAGES']:
+        return forced_locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Render the index page with parametrized templates.
+
+    Returns:
+        str: Rendered HTML content.
+    """
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
