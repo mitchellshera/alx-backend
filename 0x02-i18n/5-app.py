321352e74e3fd4a7c3f50bbcf5971417e1364230
mitchellshera <mitchellwachera@gmail.com>
2024-01-10 14:35:46 +0300

    5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -7,13 +7,6 @@ language selection, parametrized templates, and user login emulation.
 from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
 
 class Config:
     """
@@ -29,22 +22,15 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale():
-    """
-    Determine the best-matching language from the supported languages.
-
-    Returns:
-        str: Best-matching language code.
-    """
-    forced_locale = request.args.get('locale')
-    if forced_locale and forced_locale in app.config['LANGUAGES']:
-        return forced_locale
-
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
-def get_user(user_id):
+def get_user():
     """
     Get user details based on user ID.
 
@@ -54,9 +40,11 @@ def get_user(user_id):
     Returns:
         dict: User details or None if not found.
     """
-    if user_id in users:
-        return users.get(user_id)
+    id = request.args.get('login_as', None)
+    if id is not None and int(id) in users.keys():
+        return users.get(int(id))
     return None
+
 
 @app.before_request
 def before_request():
@@ -64,13 +52,23 @@ def before_request():
     Executed before all other functions.
     Find a user using get_user and set it as a global on flask.g.user.
     """
-    user_id = request.args.get('login_as')
-    g.user = None
-    if user_id:
-        user = get_user(int(user_id))
-        if user:
-            g.user = user
+    user = get_user()
+    g.user = user
 
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine the best-matching language from the supported languages.
+
+    Returns:
+        str: Best-matching language code.
+    """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
